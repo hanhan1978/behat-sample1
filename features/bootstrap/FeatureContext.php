9b5f7476741882315712e3653c325d655a76b23c
hanhan1978 <ryo.tomidokoro@gmail.com>
2015-06-24 01:59:59 +0900

    change extends to behat.yml

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,11 +5,10 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
-use Behat\MinkExtension\Context\MinkContext;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+class FeatureContext implements Context, SnippetAcceptingContext
 {
     /**
      * Initializes context.
